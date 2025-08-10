ca6acacf60f4b5e8ff8ad8d58d6f8cccafc4a003
Olzhas Mukhtarov <mihawii@Olzhass-MacBook-Air.local>
2025-08-10 19:01:57 +0500

    krutoi website

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -36,34 +36,7 @@ const results = [
 
 
 
-const containerVariants: Variants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.1,
-      ease: "easeOut",
-      duration: 0.6
-    }
-  }
-};
 
-const itemVariants: Variants = {
-  hidden: { 
-    opacity: 0, 
-    y: 30,
-    scale: 0.95
-  },
-  visible: { 
-    opacity: 1, 
-    y: 0,
-    scale: 1,
-    transition: {
-      ease: "easeOut",
-      duration: 0.5
-    }
-  }
-};
 
 interface ResultsProps {
   isDarkTheme?: boolean;
